bdd054c69290c65b39a7b5a20757453f43f05c10
lachie <lachiec@gmail.com>
2010-02-07 20:27:35 +1100

    removed logging, left fflush

--- main.c
+++ main.c
@@ -8,9 +8,6 @@
 static char separator = SEP_NEWLINE;
 static int run_once = 0;
 
-FILE *logger;
-
-
 static void callback(ConstFSEventStreamRef streamRef,
 					 void *clientCallBackInfo,
 					 size_t numEvents,
@@ -19,33 +16,23 @@ static void callback(ConstFSEventStreamRef streamRef,
 					 const FSEventStreamEventId eventIds[]) {
 
 	for(int i=0; i < numEvents; i++) {
-		fprintf(logger, "... %s\n", ((char**)eventPaths)[i]);
 		printf("%s%c", ((char**)eventPaths)[i], separator);
 	}
 
-	fprintf(logger, "post loopy\n");
 	fflush(NULL);
 	
 	if(run_once)
 		CFRunLoopStop(CFRunLoopGetMain());
-
-	fprintf(logger, "post exity\n");
-	fflush(NULL);
 }
 
 void usage() {
 	printf("Sleep until a file in or below the watchdir is modified.\n");
 	printf("Usage: fsevent_sleep /path/to/watchdir\n");
 	exit(1);
-	
 }
 
 int main (int argc, const char * argv[]) {
 	close(STDIN_FILENO);
-	
-	logger = fopen("/tmp/fes.log","w");
-	fprintf(logger, "here we go\n");
-	fflush(NULL);
 	
 	// Show help
 	if (argc < 2 || strncmp(argv[1], "-h", 2) == 0) {
@@ -71,8 +58,6 @@ int main (int argc, const char * argv[]) {
 								 latency,
 								 kFSEventStreamCreateFlagNone
 								 );
-	fprintf(logger, "starting business\n");
-	fflush(NULL);
 	
 	// Add stream to run loop
     FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
@@ -81,10 +66,6 @@ int main (int argc, const char * argv[]) {
 	
 	close(STDOUT_FILENO);
 	close(STDERR_FILENO);
-	
-	fprintf(logger, "bye\n");
-	
-	fclose(logger);
 	
 	// Exit
 	return 0;
